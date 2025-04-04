b2d5c0b1fe7193904eec2383d1036d60c77c3452
KylieAimee <kylie.farrell@edu.sait.ca>
2025-04-03 20:31:46 -0600

    add js weather and homepage html

---
+++ homepage.js
@@ -0,0 +1,10 @@
+fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=327bcddfebe7b2e3b2f5f4cd270c64a5&units=metric'
+)
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    console.log(data.main.temp);
+    weather.textContent = "Cancun, Mexico "+data.main.temp+"\u00B0C";
+});
